edcd2826fcefabc615f9ae4cb6c931e075cae54f
Rodrigo Silverio <rodrigo.s.silverio@outlook.comgit config --global user.email rodrigo.s.silverio@outlook.com>
2024-09-23 13:05:56 -0700

    variavel de ambiente

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 // import { env } from '../env'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 })
